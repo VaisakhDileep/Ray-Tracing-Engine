bcae5bca22e99ecfa600e60de9de6ae81a163143
Vaisakh Dileep <vaisakhdill@gmail.com>
2021-10-10 15:48:28 +0530

    Rectified a small bug in ../src/colour/colour.hpp

--- src/colour/colour.hpp
+++ src/colour/colour.hpp
@@ -13,9 +13,9 @@ Description : Helper functions for writing colour to the p3 file(declaration).
 
 using namespace std;
 
-void write_colour_0_1_format(ofstream &out_file, const colour_3d pixel, double gama_correction = 1);
+void write_colour_0_1_format(ofstream &out_file, const colour_3d pixel, double gama_correction);
 
-void write_colour_0_1_format(ofstream &out_file, const colour_3d pixel, const int samples_per_pixel, double gama_correction = 1);
+void write_colour_0_1_format(ofstream &out_file, const colour_3d pixel, const int samples_per_pixel, double gama_correction);
 
 void write_colour_0_255_format(ofstream &out_file, const colour_3d pixel); // For 0..255 format, we are ignoring gama correction.
 
