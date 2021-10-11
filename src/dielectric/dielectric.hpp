eadaf1b83cf81a125647dfe30ea523df24b38b84
Vaisakh Dileep <vaisakhdill@gmail.com>
2021-10-11 20:56:27 +0530

    Added dielectric to src.

---
+++ src/dielectric/dielectric.hpp
@@ -0,0 +1,26 @@
+/*
+Created by  : Vaisakh Dileep
+Date        : 11, October, 2021
+Description : Class declaration for the dielectric class.
+*/
+
+#ifndef _DIELECTRIC_HPP_
+#define _DIELECTRIC_HPP_
+
+#include "../material/material.hpp"
+
+using namespace std;
+
+class dielectric: public material
+{
+public:
+    double refractive_index;
+
+    dielectric(); // No-args constructor
+
+    dielectric(double index_of_refraction);
+
+    virtual bool scatter(const ray &incident_ray, const hit_record &record, colour_3d &attenuation, ray &scattered_ray, double refractive_index_incident_material) const override;
+};
+
+#endif
