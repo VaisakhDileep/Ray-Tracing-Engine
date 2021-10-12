4e9f5dc797a87de355d765692f44aa7d35fe6972
Vaisakh Dileep <vaisakhdill@gmail.com>
2021-10-12 10:57:40 +0530

    Made some small adjustments to src/sphere.cpp

--- src/sphere/sphere.cpp
+++ src/sphere/sphere.cpp
@@ -47,11 +47,9 @@ bool sphere::hit(const ray &r, double t_min, double t_max, hit_record &record) c
 
             record.p = r.point_at_parameter(solution);
 
-            record.normal = (r.point_at_parameter(solution) - center).make_unit_vector();
-
             record.material_ptr = material_ptr;
 
-            record.set_face_normal(r, record.normal);
+            record.set_face_normal(r, (r.point_at_parameter(solution) - center).make_unit_vector());
 
             return true;
         }
