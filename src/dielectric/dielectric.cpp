ec87d9d763035105a1645418b927bbcfc426c99e
Vaisakh Dileep <vaisakhdill@gmail.com>
2021-10-12 09:58:37 +0530

    Added refractive_index_external_medium attribute to src/dielectric.

--- src/dielectric/dielectric.cpp
+++ src/dielectric/dielectric.cpp
@@ -13,16 +13,21 @@ Description : Class definition for the dielectric class.
 using namespace std;
 
 dielectric::dielectric() // No-args constructor
-    : refractive_index {0.0}
+    : refractive_index_medium {0.0}
 {
 }
 
-dielectric::dielectric(double index_of_refraction) // Overloaded constructor
-    : refractive_index {index_of_refraction}
+dielectric::dielectric(double refractive_index_medium) // Overloaded constructor
+    : refractive_index_medium {refractive_index_medium}
 {
 }
 
-bool dielectric::scatter(const ray &incident_ray, const hit_record &record, colour_3d &attenuation, ray &scattered_ray, double refractive_index_external_material = 1.0) const
+dielectric::dielectric(double refractive_index_medium, double refractive_index_external_medium) // Overloaded constructor
+    : refractive_index_medium {refractive_index_medium}, refractive_index_external_medium {refractive_index_external_medium}
+{
+}
+
+bool dielectric::scatter(const ray &incident_ray, const hit_record &record, colour_3d &attenuation, ray &scattered_ray) const
 {
     attenuation = colour_3d {1, 1, 1};
 
@@ -30,11 +35,11 @@ bool dielectric::scatter(const ray &incident_ray, const hit_record &record, colo
 
     if(record.outer_face == true)
     {
-        relative_refractive_index = refractive_index_external_material / refractive_index;
+        relative_refractive_index = refractive_index_external_medium / refractive_index_medium;
     }
     else
     {
-        relative_refractive_index = refractive_index / refractive_index_external_material;
+        relative_refractive_index = refractive_index_medium / refractive_index_external_medium;
     }
 
     vector_3d refracted_ray_direction {refract(incident_ray.direction(), record.normal, relative_refractive_index)};
