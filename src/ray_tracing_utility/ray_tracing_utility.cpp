60572cd3d3b70450c562ee929e383f8f0ae2575e
Vaisakh Dileep <vaisakhdill@gmail.com>
2021-10-13 12:14:56 +0530

    Made some minor cosmetic changes and resolved some bugs.

--- src/ray_tracing_utility/ray_tracing_utility.cpp
+++ src/ray_tracing_utility/ray_tracing_utility.cpp
@@ -59,17 +59,17 @@ vector_3d reflect(const vector_3d &incident_ray, const vector_3d &normal)
     return incident_ray - 2 * (dot(incident_ray, normal) * normal); // "dot()" will return a '-ve' value.
 }
 
-vector_3d refract(const vector_3d &incident_ray, const vector_3d &normal, double relative_refractive_index) // "relative_refractive_index" = (refractive index of tranmitted medium / refractive index of incident medium).
+vector_3d refract(const vector_3d &incident_ray_direction, const vector_3d &normal, double relative_refractive_index) // "relative_refractive_index" = (refractive index of tranmitted medium / refractive index of incident medium).
 {
-// normal will always be a unit_vector in my ray-tracing engine, so need to handle it.
+// normal will always be a unit_vector in my ray-tracing engine, so no need to handle it.
 
-    vector_3d incident_ray_unit_vector {incident_ray.unit_vector()};
+    vector_3d incident_ray_unit_vector {incident_ray_direction.unit_vector()};
 
     double cos_incident_angle {dot(-incident_ray_unit_vector, normal)}; // We reverse "incident_ray_unit_vector" so that the angle between "incident_ray_unit_vector" and "normal" is actute.
 
     vector_3d transmitted_ray_component_perpendicular_to_normal {relative_refractive_index * (incident_ray_unit_vector + cos_incident_angle * normal)}; // Will be tangential to the normal.
 
-    vector_3d transmitted_ray_component_parallel_to_normal {-sqrt(1.00 - transmitted_ray_component_perpendicular_to_normal.squared_length()) * normal}; // Will be in the same direction as that of normal.
+    vector_3d transmitted_ray_component_parallel_to_normal {-sqrt(fabs(1.00 - transmitted_ray_component_perpendicular_to_normal.squared_length())) * normal}; // Will be in the same direction as that of normal.
 
     return transmitted_ray_component_perpendicular_to_normal + transmitted_ray_component_parallel_to_normal;
 }
