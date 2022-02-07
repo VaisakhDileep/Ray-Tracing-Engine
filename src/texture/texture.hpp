81f04cd8fa2e44583524d1fd81d3f7f7da447313
Vaisakh Dileep <vaisakhdill@gmail.com>
2022-02-07 21:09:09 +0530

    Added src/texture.hpp

---
+++ src/texture/texture.hpp
@@ -0,0 +1,18 @@
+/*
+Created by  : Vaisakh Dileep
+Date        : 7, February, 2022
+Description : Class definition for the texture class.
+*/
+
+#ifndef _TEXTURE_HPP_
+#define _TEXTURE_HPP_
+
+#include "../vector_3d/vector_3d.hpp"
+
+class texture
+{
+public:
+    virtual colour_3d colour_output(double u, double , const point_3d &p) const = 0;
+};
+
+#endif
